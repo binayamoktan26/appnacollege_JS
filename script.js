6c87a25e4c955a4bfdc52117a8d17e561c153807
nawang tashi lama <binayamoktan26@gmail.com>
2025-02-22 22:49:23 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// const profile = {
+//   userName: "shraddakhapra",
+//   isFollow: true,
+//   followers: 23,
+//   following: 34,
+// };
+// console.log(typeof profile.isFollow);
+
+//operators and conditional statements
+/*write  a code which can give  grades to students according to their scores .
+1 80-100  A
+2 70-79  B
+3 60-69  C
+4 50-59  D
+5 0-49  F
+
+
+Answer : */
+
+// let score = 53;
+// if (score >= 80 && score <= 100) {
+//   console.log("A");
+// } else if (score >= 70 && score <= 79) {
+//   console.log("B");
+// } else if (score >= 60 && score <= 69) {
+//   console.log("C");
+// } else if (score >= 50 && score <= 59) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// for-of Loop
+// let str = "javascript";
+// let size = 0;
+// for (let val of str) {
+//   console.log(val);
+//   size++;
+// }
+
+// console.log(size);
+//for-in Loop
+// let student = {
+//   name: "shraddha",
+//   Age: 22,
+//   cgpa: 5.3,
+//   isPass: true,
+// };
+// for (let key in student) {
+//   console.log("key=", key, "value=", student[key]);
+// }
+
+/* let's practice 
+1. Print all even numbers from 0 to 100?
+*/
+
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i++)
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+
+// let gameNum = 26;
+// let usrNum = prompt("guess the number ");
+// while (usrNum != gameNum) {
+//   usrNum = prompt("you entered wrong number, try again");
+// }
+// console.log("congratulation you entered correct number");
+
+
